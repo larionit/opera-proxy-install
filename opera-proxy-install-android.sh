79c75eb8aba9b1603a4ba9247f3677b242df0d4d
larionit <illarionit@gmail.com>
2024-09-04 21:21:32 +0500

    Release the kraken!

--- opera-proxy-install-android.sh
+++ opera-proxy-install-android.sh
@@ -4,25 +4,31 @@ repo=opera-proxy
 repo_owner=Snawoot
 target_dir="${PATH}"
 
-# Array with package names
+# Array with required packages names
 dependency_packages=(
   "curl"
   "jq"
 )
 
-# Array loop
-for package in "${dependency_packages[@]}"; do
-  if ! command -v "$file" &> /dev/null; then
-    echo "File '$package' was not found. Installation..."
-    pkg install -y "$package"
-    if [[ $? -eq 0 ]]; then
-      echo "Installation of '$package' completed successfully."
+clear
+echo
+echo "~> opera-proxy-install-android.sh"
+echo
+
+# Installing the required packages
+for package in "${dependency_packages[@]}"
+do
+    if [[ ! -f "$target_dir/$package" ]]; then
+        echo "Package '$package' was not found. Installation..."
+        pkg install -y "$package" &> /dev/null 
+            if [[ $? -eq 0 ]]; then
+                echo "Installation of '$package' completed successfully."
+            else
+                echo "Error installing '$package'."
+            fi
     else
-      echo "Error installing '$package'."
+        echo "Package '$package' is already installed."
     fi
-  else
-    echo "The file '$package' is already installed."
-  fi
 done
 
 # Getting information about the kernel
@@ -30,9 +36,9 @@ uname_output=$(uname -a)
 
 # Determine the processor architecture based on information about the current kernel
 if [[ "$uname_output" = *aarch64* ]]; then
-  target_file=opera-proxy.android-arm64
+    target_file=opera-proxy.android-arm64
 else
-  target_file=opera-proxy.android-arm
+    target_file=opera-proxy.android-arm
 fi
 
 # Remove platform information from the executable file name
@@ -47,32 +53,36 @@ install_release="${latest_release}"
 # Set the link to download the target file
 download_link=https://github.com/$repo_owner/$repo/releases/download/$install_release/$target_file
 
-# Look for the executable file on the device, if the file is found: get its version and delete it.
+# Checking if an opera-proxy is already installed
 if [[ -f $target_dir/$opera_proxy_bin ]]; then
-  current_version=$($opera_proxy_bin --version)
-  echo "Version found on the device: $current_version"
-  rm $target_dir/$opera_proxy_bin
+    current_version=$($opera_proxy_bin --version)
+    echo "Version found on the device: $current_version"
+        if [[ "$current_version" = "$install_release" ]]; then
+                clear
+                echo
+                echo "~> opera-proxy-install-android.sh"
+                echo
+                echo "Nothing to do!"
+                echo
+                echo "The target version is already installed: $current_version"
+                echo
+                echo "Command to start proxy: $opera_proxy_bin"
+                echo
+                exit 0
+        else
+            rm $target_dir/$opera_proxy_bin
+        fi
 fi
 
-if [[ ! "$current_version" = "$install_release" ]]; then
-  echo "Loading Release: $install_release"
-  # Downloading specified release
-  curl -fsSL $download_link -o $target_dir/$opera_proxy_bin  
-  # Grant execute permission
-  chmod +x $target_dir/$opera_proxy_bin
-
-  echo
-  echo "Installed version: $current_version"
-  echo
-  echo "Command to start proxy: $opera_proxy_bin"
-  echo
-else
-  echo
-  echo "Latest release on github: $latest_release"
-  echo
-  echo "Installed version: $install_release"
-  echo
-  echo "Command to start proxy: $opera_proxy_bin"
-  echo
-fi
-
+# Download and install
+echo "Loading Release: $install_release"
+curl -fsSL $download_link -o $target_dir/$opera_proxy_bin  
+chmod +x $target_dir/$opera_proxy_bin
+clear
+echo
+echo "~> opera-proxy-install-android.sh"
+echo
+echo "Installed version: $install_release"
+echo
+echo "Command to start proxy: $opera_proxy_bin"
+echo
