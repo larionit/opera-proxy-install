cb8c4d52d7932981bbb5f8556b17df8e4f45de93
larionit <illarionit@gmail.com>
2024-09-04 19:49:57 +0500

    Added installation of packages required for the script to work.

--- opera-proxy-install-android.sh
+++ opera-proxy-install-android.sh
@@ -4,22 +4,35 @@ repo=opera-proxy
 repo_owner=Snawoot
 target_dir="${PATH}"
 
-# Define the directory where this script is located
-script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+# Array with package names
+dependency_packages=(
+  "curl"
+  "jq"
+)
 
-# For testing
-if [[ ! "$script_dir" = "/data/data/com.termux/files/home" ]]; then
-    echo $script_dir
-fi
+# Array loop
+for package in "${dependency_packages[@]}"; do
+  if [[ ! command -v "$package" &> /dev/null]]; then
+    echo "File '$package' was not found. Installation..."
+    pkg install -y "$package"
+    if [[ $? -eq 0 ]]; then
+      echo "Installation of '$package' completed successfully."
+    else
+      echo "Error installing '$package'."
+    fi
+  else
+    echo "The file '$package' is already installed."
+  fi
+done
 
 # Getting information about the kernel
 uname_output=$(uname -a)
 
 # Determine the processor architecture based on information about the current kernel
 if [[ "$uname_output" = *aarch64* ]]; then
-    target_file=opera-proxy.android-arm64
+  target_file=opera-proxy.android-arm64
 else
-    target_file=opera-proxy.android-arm
+  target_file=opera-proxy.android-arm
 fi
 
 # Remove platform information from the executable file name
@@ -34,32 +47,32 @@ install_release="${latest_release}"
 # Set the link to download the target file
 download_link=https://github.com/$repo_owner/$repo/releases/download/$install_release/$target_file
 
-# Search for the executable file on the device and check its version
+# Look for the executable file on the device, if the file is found: get its version and delete it.
 if [[ -f $target_dir/$opera_proxy_bin ]]; then
-    current_version=$($opera_proxy_bin --version)
-    echo "Version found on the device: $current_version"
+  current_version=$($opera_proxy_bin --version)
+  echo "Version found on the device: $current_version"
+  rm $target_dir/$opera_proxy_bin
 fi
 
 if [[ ! "$current_version" = "$install_release" ]]; then
-    echo "Loading Release: $install_release"
-    # Downloading specified release
-    curl -fsSL $download_link -o $target_dir/$opera_proxy_bin
+  echo "Loading Release: $install_release"
+  # Downloading specified release
+  curl -fsSL $download_link -o $target_dir/$opera_proxy_bin  
+  # Grant execute permission
+  chmod +x $target_dir/$opera_proxy_bin
 
-    # Grant execute permission
-    chmod +x $target_dir/$opera_proxy_bin
-    
-    echo
-    echo "Installed version: $current_version"
-    echo
-    echo "Command to start proxy: $opera_proxy_bin"
-    echo
+  echo
+  echo "Installed version: $current_version"
+  echo
+  echo "Command to start proxy: $opera_proxy_bin"
+  echo
 else
-    echo
-    echo "Latest release on github: $latest_release"
-    echo
-    echo "Installed version: $install_release"
-    echo
-    echo "Command to start proxy: $opera_proxy_bin"
-    echo
+  echo
+  echo "Latest release on github: $latest_release"
+  echo
+  echo "Installed version: $install_release"
+  echo
+  echo "Command to start proxy: $opera_proxy_bin"
+  echo
 fi
 
